044958bb525e3731e1d5bdbb2c8866f7c922acfb
Rodrigo <rodrigomontijano@gmail.com>
2022-10-03 05:18:07 -0300

    páginas menu e consultar-produto

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(document).keypress(function (event) {
+        var bindList = [
+            {key: 109, page: "menu.html"},
+            {key: 99, page: "consultar-produto.html"},
+        ];
+        
+        var key = event.which;
+        var bind = bindList.find((i) => i.key == key);
+
+        console.log(key);
+
+        if(bind) {
+            window.location.replace(bind.page);
+        }
+    });
+});
